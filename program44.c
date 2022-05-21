2b98f0b0a6d8dc054376df1cdcdf7f09c9996a91
raskarabhi45 <88926036+raskarabhi45@users.noreply.github.com>
2022-05-20 20:00:30 -0700

    Add files via upload

---
+++ program44.c
@@ -0,0 +1,48 @@
+//copy of program39.c..but without flag
+//Best logic of prime number
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPrime(int iNo)
+{
+    int iCnt=0;
+    if(iNo<0)
+    {
+        iNo=-iNo;
+    }
+    for(iCnt=2;iCnt<=iNo/2;iCnt++)
+    {
+        if(iNo%iCnt==0)
+        { 
+           break;
+        }
+    }
+    if(iCnt==(iNo/2)+1)   // logic
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+ 
+}
+
+int main()
+{
+    int iValue=0;
+    bool bRet;
+
+    printf("Enter numeber\n");
+    scanf("%d",&iValue);
+    bRet=CheckPrime(iValue);
+
+    if(bRet==true)
+    {
+        printf("%d is prime number\n",iValue);
+    }
+    else{
+        printf("%d is not prime number\n",iValue);
+    }
+
+
+}
